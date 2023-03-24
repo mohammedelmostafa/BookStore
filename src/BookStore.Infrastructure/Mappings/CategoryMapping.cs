2f4c7486db415fd2de72413a73e7ee258c487f26
Mohammed Elmostafa Moawia <m.elmostafa@yeel.io>
2023-03-24 16:24:39 +0400

    add DBContext and Mapping and Models

---
+++ src/BookStore.Infrastructure/Mappings/CategoryMapping.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BookStore.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace BookStore.Infrastructure.Mappings
+{
+    public class CategoryMapping:IEntityTypeConfiguration<Category>
+    {
+        public void Configure(EntityTypeBuilder<Category> builder)
+        {
+            builder.HasKey(c => c.Id);
+            builder.Property(c => c.Name).IsRequired().HasColumnType("varchar(150)");
+            builder.HasMany(c=>c.Books).WithOne(b=>b.Category).HasForeignKey(b => b.CategoryId);
+
+            builder.ToTable("Categories");
+        }
+    }
+}
